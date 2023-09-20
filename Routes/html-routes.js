7693542e06eaf30cfae40ceaa56d20af1b436559
halleklum <halleklum@gmail.com>
2023-09-20 14:17:06 -0400

    tweaks to fix functionality

--- Routes/html-routes.js
+++ Routes/html-routes.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 const path = require('path');
 
-// Route that sends 'index.html' as a response to a client when a GET request is made
+// Defines the route that sends 'index.html' as a response to a client when a GET request is made
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 });
-
 // Route that sends 'notes.html" as a response to a client when a GET request is made 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
